dcc5eb21359845564ac9a6d9a06a105c86fc9bbb
King Art <arthur@ischium.net>
2026-02-13 13:15:13 +0000

    feat: build and install man page
    The `artstd` package now builds a man page from `README.md` and
    installs it into the derivation output.
    
    This makes the engineering standards available as a system manual page.
    
    Changes:
    - `default.nix`:
      - Added `pandoc` as a `nativeBuildInput`.
      - Implemented a `buildPhase` to convert `README.md` to a man page (`artstd.1`) after substituting the git revision.
      - Updated the `installPhase` to install the man page into `$out/share/man/man1` and the substituted README into `$out/share/doc/artstd`.
      - Corrected the use of `${pname}` to `$pname` to properly reference the environment variable set by `stdenv`.

--- default.nix
+++ default.nix
@@ -2,24 +2,43 @@
   lib,
   stdenv,
   rev ? "dirty",
+  pandoc,
 }:
 stdenv.mkDerivation {
 
   pname = "artstd";
 
-  version = "dev";
+  version = rev;
 
   src = lib.fileset.toSource {
     root = ./.;
     fileset = lib.fileset.unions [ ./README.md ];
   };
 
-  dontBuild = true;
+  nativeBuildInputs = [ pandoc ];
+
+  buildPhase = ''
+    runHook preBuild
+
+    # Substitute rev and create man page
+    sed "s/@REV@/${rev}/g" README.md > README.rev.md
+    pandoc -s README.rev.md --metadata=title:"artstd" -t man -o artstd.1
+
+    runHook postBuild
+  '';
 
   installPhase = ''
-    mkdir -p $out
-    cp README.md $out/README.md
-    sed -i "s/@REV@/${rev}/g" $out/README.md
+    runHook preInstall
+
+    # Install man page
+    mkdir -p $out/share/man/man1
+    install -m 444 artstd.1 $out/share/man/man1/
+
+    # Install README
+    mkdir -p $out/share/doc/$pname
+    install -m 444 README.rev.md $out/share/doc/$pname/README.md
+
+    runHook postInstall
   '';
 
 }
